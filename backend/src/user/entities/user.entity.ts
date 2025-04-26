83efa927ddad7237a3cf69ac513f74c5a0a40b03
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2025-04-26 15:17:35 -0300

    User: Add user resource

---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
